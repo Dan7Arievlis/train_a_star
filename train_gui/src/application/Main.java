7bad4cbbaa3479a0ecca1f81215351440779eba7
Dan7Arievlis <danbrasileir07@gmail.com>
2022-04-06 15:56:40 -0300

    Canvas funcionando

--- train_gui/src/application/Main.java
+++ train_gui/src/application/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -13,20 +14,28 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+  @FXML
+  Canvas lineCanvas;
+//  GraphicsContext gc;
+  
+  boolean running = true;
+
   @Override
   public void start(Stage primaryStage) {
     primaryStage.setTitle("Timeline Example");
 
     try {
       AnchorPane root = FXMLLoader.load(getClass().getResource("scenes/Main.fxml"));
-
+      
+      Canvas canvas = new Canvas(750, 430);
+      root.getChildren().add(canvas);
+      
       Scene theScene = new Scene(root);
       primaryStage.setScene(theScene);
 
-      Canvas canvas = new Canvas(root.getWidth(), root.getHeight());
       primaryStage.setResizable(false);
-      
-      GraphicsContext gc = canvas.getGraphicsContext2D();
+
+      GraphicsContext gc =  canvas.getGraphicsContext2D();
 
       final long startNanoTime = System.nanoTime();
 
@@ -38,14 +47,13 @@ public class Main extends Application {
           // TODO: Quero usar isso pra mudar a cor da aresta a depender do estado se a
           // aresta foi visitada ou não no percurso
           Color color = (currentNanoTime / 100000000) % 100 < 40 ? Color.YELLOW : Color.AQUA;
-          gc.setFill(color);
+          gc.setStroke(color);
           gc.setLineWidth(4);
-          gc.fillRect(10, 20, 10, 300);
+          gc.strokeLine(10, 0, 10, 300);
         }
       }.start();
 
       primaryStage.show();
-      root.getChildren().add(canvas);
     } catch (IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
